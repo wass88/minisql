a25b4a0380662ac2568f397ca4e663be66786c43
wass80 <wasss80@gmail.com>
2023-05-08 13:13:33 +0900

    simple commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,72 @@
 fn main() {
-    let mut buf = String::new();
     loop {
-        std::io::stdin()
-            .read_line(&mut buf)
-            .expect("Failed to read line");
-        if buf.trim() == ".exit" {
-            break;
+        let mut buf = String::new();
+        if let Err(e) = std::io::stdin().read_line(&mut buf) {
+            println!("Error reading input: {}", e);
+            continue;
+        }
+
+        let buf = buf.trim();
+        if buf.starts_with(".") {
+            match meta_command(buf) {
+                Ok(_) => continue,
+                Err(_) => {
+                    println!("Unrecognized command '{}'.", buf);
+                    continue;
+                }
+            }
+        }
+        let statement = prepare_statement(buf);
+        if let Err(_) = statement {
+            println!("Unrecognized keyword at start of '{}'.", buf);
+            continue;
+        }
+        let statement = statement.unwrap();
+        if let Err(_) = statement.execute() {
+            println!("Error executing statement.");
+            continue;
+        }
+    }
+}
+
+fn meta_command(buf: &str) -> Result<(), ()> {
+    match buf {
+        ".exit" => {
+            std::process::exit(0);
+        }
+        _ => {
+            return Err(());
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Statement {
+    Insert,
+    Select,
+}
+
+fn prepare_statement(buf: &str) -> Result<Statement, ()> {
+    if buf.starts_with("insert") {
+        return Ok(Statement::Insert);
+    }
+    if buf.starts_with("select") {
+        return Ok(Statement::Select);
+    }
+    Err(())
+}
+
+impl Statement {
+    fn execute(&self) -> Result<(), ()> {
+        match self {
+            Statement::Insert => {
+                println!("This is where we would do an insert.");
+                Ok(())
+            }
+            Statement::Select => {
+                println!("This is where we would do a select.");
+                Ok(())
+            }
         }
     }
 }
